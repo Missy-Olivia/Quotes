46ef12394feb3b5eb11b89f809d7bef3b21f620d
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-02 18:49:24 +0200

    added form for new quotes

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote-form',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  quoted = new Quote(0,"","",new Date(),0,0);
+  @Output() add = new EventEmitter<Quote>();
 
+  addQuote(){
+this.add.emit(this.quoted);
+this.quoted = new Quote(0,"","",new Date(),0,0);
+  }
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+
   }
 
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,7 +9,7 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
   quotes:Quote[] = [
     new Quote (1,'If music be the food of love, play on.','William shakespeare ', new Date(2020,11,2),0,0),
-    new Quote (2,'If you can dream it, you can do it. Walt Disney','Walt Disney',new Date(2020,11,1),0,0),
+    new Quote (2,'If you can dream it, you can do it. ','Walt Disney',new Date(2020,11,2),0,0),
     new Quote (3,'Start by doing what is necessary then do what is possible; and suddenly you are doing the impossible.','Francis of Assisi',new Date(2020,10,31),0,0),
     new Quote (4,'What lies behind you and what lies in front of you, pales in comparison to what lies inside of you.','Ralph Waldo Emerson',new Date(2020,11,2),0,0),
     new Quote (5,'We know what we are, but know not what we may be.','William shakespeare',new Date(2020,11,2),0,0),
