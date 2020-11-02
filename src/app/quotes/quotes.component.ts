982b6f78f111806718812b2f0bb364a989a16b41
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-02 05:26:47 +0200

    adde quote output  proprties

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { doesNotReject } from 'assert';
 import { Quote } from '../quote';
 
 @Component({
@@ -8,7 +7,6 @@ import { Quote } from '../quote';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-  title = 'Quote Hub';
   quotes:Quote[] = [
     new Quote (1,'If music be the food of love, play on.','William shakespeare ', new Date(2020,11,2),0,0),
     new Quote (2,'If you can dream it, you can do it. Walt Disney','Walt Disney',new Date(2020,11,1),0,0),
@@ -17,7 +15,30 @@ export class QuotesComponent implements OnInit {
     new Quote (5,'We know what we are, but know not what we may be.','William shakespeare',new Date(2020,11,2),0,0),
     new Quote (6,'If opportunity does not knock, build a door.','Milton Berle',new Date(2020,11,2),0,0)
   ];
-
+  get sortQuotes() {
+    return this.quotes.sort((a, b) => {
+      return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
+    });
+  }  
+  addedQuote(quote){
+    let arraysize = this.quotes.length;
+    quote.id = arraysize+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  quoteDelete(isRead, index){
+    if (isRead) {
+      let toDelete = confirm(`Are you sure you want to delete this Quote?`)
+      if(toDelete){
+        this.quotes.splice(index,1);
+      }
+      
+    }
+  }
+ 
+  displayInfo(index){
+    this.quotes[index].showDets = !this.quotes[index].showDets;
+  }
   constructor() { }
 
   ngOnInit(): void {
